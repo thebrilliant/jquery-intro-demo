913741aecf1275ec8ec08872ada3d12bc54d6e50
viv <vivyanw@uw.edu>
2014-10-30 09:29:13 -0700

    working on demo stuff

--- js/app.js
+++ js/app.js
@@ -4,6 +4,41 @@
 
 "use strict";
 
+var tile = 'img/sec1.jpg';
+var info = 'some other data';
+
+var gameBoard = $('#game-board');
+
+//create and configure a new img element
+var newTile = $(document.createElement('img'));
+
+newTile.attr('src', 'img/background.jpg');
+newTile.attr('alt', 'photo of nature');
+newTile.attr('width', '250px');
+
+//use the .data() method to associate extra data with HTML elements
+newTile.data('assocTile', tile); //first param is key
+newTile.data('tileInfo', info);
+
+//add to the game board
+gameBoard.append(newTile);
+
+//when an image in gameboard is clicked
+$('#game-board img').click(function() {
+	//remember that 'this' refers to the DOM element that raised
+	//the event; wrapped in jQuery $() to get more functionality
+	var clickedImage = $(this);
+	var tileData1 = clickedImage.data('assocTile');
+	var tileData2 = clickedImage.data('tileInfo');
+
+	clickedImage.attr('src', tileData1);
+	clickedImage.attr('alt', tileData2);
+
+	console.log(tileData1);
+	console.log(tileData2);
+});
+
+/*
 //select all navigation links
 var allNavLinks = $('nav a');
 
@@ -59,3 +94,4 @@ allNavLinks.click(function() {
 });
 
 $(onReady);
+*/
